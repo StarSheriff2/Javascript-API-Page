8f6ab22314dc5b7a2ec4f2d991eaefe340205789
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-04 15:07:33 -0600

    add total number of items to current page at the top

--- src/app/run.js
+++ src/app/run.js
@@ -1,8 +1,9 @@
 /* eslint-disable linebreak-style */
-import ObjectRenderer from './DOM/dom_functions';
+import { ObjectRenderer, showItemsCounter } from './DOM/dom_functions';
 import likes from './objects/likes';
 
 export default async function run() {
   await likes.mapLikes();
-  ObjectRenderer.renderElements('monsters');
+  await ObjectRenderer.renderElements('monsters');
+  showItemsCounter();
 }
