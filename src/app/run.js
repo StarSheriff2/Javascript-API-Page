6c09a3b5265f8827e17c1f498287a8f82591a0b4
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-04 19:15:27 -0600

    fix issue with bug in run.js

--- src/app/run.js
+++ src/app/run.js
@@ -9,5 +9,4 @@ export default async function run() {
   await likes.mapLikes();
   await ObjectRenderer.renderElements('monsters');
   showItemsCounter();
-  ObjectRenderer.renderElements('monsters');
 }
